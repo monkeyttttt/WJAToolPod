fdb82c17dda94995246ce00e49f9a69b0f8b3b61
chenliang <>
2022-04-27 16:44:53 +0800

    描述文件

---
+++ Classes/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  test_unit
+//
+//  Created by chenliang on 2022/4/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+@property(nonatomic,strong)NSString *name;
+@property(nonatomic,assign)int age;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/Person.m
@@ -0,0 +1,12 @@
+//
+//  Person.m
+//  test_unit
+//
+//  Created by chenliang on 2022/4/22.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
